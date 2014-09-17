610f176c066ff53a592d7f863814b6c6f23fab5c
bjlinville1 <Jgrant@Jennifers-MacBook-Pro.local>
2014-09-17 16:44:20 -0400

    Merge branch 'master' of https://github.com/TIY-Q3-ROR-CHS/Team3
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, presence: true
+
+ include Workflow
+  workflow do
+
+    state :unavailable do
+     event :become_available, transitions_to: :available
+    end 
+    
+    state :available do
+     event :get_busy, transitions_to: :unavailable
+    end
+  end
+
 end
