5ca112c96dda90f39d5c34e60855e9ae97141ecc
Belinda Hare <BelindaHare@Belindas-MacBook-Air.local>
2014-09-17 14:02:13 -0400

    added friendship database, added columns to friendship database, added user and friendship controllers, added resources in routes.rb, added models for these,  all of this will give us the ability to connect users and create friends list

---
+++ app/controllers/friendship_controller.rb
@@ -0,0 +1,26 @@
+class FriendshipController < ApplicationController
+  def index
+  end
+  
+  def new
+    @friendship = Friendship.new
+  end
+  def create
+  @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+  if @friendship.save
+    flash[:notice] = "Friend added to @home list!"
+    redirect_to root_url
+  else
+    flash[:error] = "Error adding friend to your @home list"
+    redirect_to root_url
+  end
+end
+
+def destroy
+  @friendship = current_user.friendships.find(params[:id])
+  @friendship.destroy
+  flash[:notice] = "Removed friend from your @home list!"
+  redirect_to current_user
+end
+end
+#this controller is giving us the ability to create relationships between the users.  It also gives users the ability to add and delete people from their friends list
