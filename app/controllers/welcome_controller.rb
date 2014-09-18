5a883edb20c8c8ac6a192fbcb67eeadeb32ae0bc
Belinda Hare <BelindaHare@Belindas-MacBook-Air.local>
2014-09-18 11:05:27 -0400

    friendship controller fix for add friend link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @users = User.all
-    @friendships = Friendship.all
+  
     #need this to display the users on the welcome index page
   end
   private
